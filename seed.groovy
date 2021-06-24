8a336d43b1738ec4777e9652e3f9e5f0bfa2384b
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-06-24 17:04:53 +0530

    groovy

--- seed.groovy
+++ seed.groovy
@@ -63,43 +63,6 @@ pipelineJob("CI-Pipelines/frontend-ci") {
 }
 
 
-//def component = ["cart", "catalogue","user","payment","shipping","frontend"];
-
-//def count=(component.size()-1)
-//for (i in 0..count) {
-//    def j=component[i]
-//    pipelineJob("CI-Pipelines/${j}-ci") {
-//        configure { flowdefinition ->
-//            flowdefinition / 'properties' << 'org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty' {
-//                'triggers' {
-//                    'hudson.triggers.SCMTrigger' {
-//                        'spec'('*/2 * * * 1-5')
-//                        'ignorePostCommitHooks'(false)
-//                    }
-//                }
-//            }
-//            flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-//                'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-//                    'userRemoteConfigs' {
-//                        'hudson.plugins.git.UserRemoteConfig' {
-//                            'url'('https://github.com/zsdevops01/'+j+'.git')
-//                            'refspec'('\'+refs/tags/*\':\'refs/remotes/origin/tags/*\'')
-//                        }
-//                    }
-//                    'branches' {
-//                        'hudson.plugins.git.BranchSpec' {
-//                            'name'('*/tags/*')
-//                        }
-//                    }
-//
-//
-//               }
-//                'scriptPath'('Jenkinsfile')
-//                'lightweight'(true)
-//            }
-//        }
-//    }
-//}
 
 pipelineJob("CI-Pipelines/users-ci") {
     configure { flowdefinition ->
