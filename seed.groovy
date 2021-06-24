6569a0c43087180e735ee5c86a574bad440ecf17
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-06-24 17:13:29 +0530

    Merge remote-tracking branch 'origin/main' into main
    # Conflicts:
    #	Jenkinsfile

--- seed.groovy
+++ seed.groovy
@@ -86,3 +86,27 @@ pipelineJob("CI-Pipelines/users-ci") {
 }
 
 
+
+
+pipelineJob("CI-Pipelines/todo-ci") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/dev0ps01/todo.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile')
+            'lightweight'(true)
+        }
+    }
+}
+
+
