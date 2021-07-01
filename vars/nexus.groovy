e17b0a11f7d2d2d57e727058609ed05294e9ae31
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-07-01 18:14:06 +0530

    groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -2,7 +2,7 @@ def nexus (COMPONENT) {
     get_branch = "env | grep GIT_BRANCH | awk -F / '{print \$NF}' | xargs echo -n"
     def get_branch_exec=sh(returnStdout: true, script: get_branch)
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
-    command = "curl -f -v -u admin:vamsi --upload-file ${FILENAME} http://172.31.9.137:8081/repository/${COMPONENT}1/${FILENAME}"
+    command = "curl -f -v -u admin:vamsi --upload-file ${FILENAME} http://172.31.9.137:8081/repository/${COMPONENT}/${FILENAME}"
     def execute_state=sh(returnStdout: true , script: command)
 }
 def artifacts(APP_TYPE,COMPONENT) {
@@ -12,46 +12,46 @@ def artifacts(APP_TYPE,COMPONENT) {
     def FILENAME=COMPONENT+'-'+get_branch_exec+'.zip'
     if(APP_TYPE == "NGINX" ) {
         command = " zip -r ${FILENAME} node_modules dist"
-        def execute_com= sh(returnStdout: true, script: command)
-        print execute_com
+        def execute= sh(returnStdout: true, script: command)
+        print execute
     }
     else if(APP_TYPE == "NODEJS" ) {
         command = "zip -r ${FILENAME} node_modules server.js"
-        def execute_com= sh(returnStdout: true, script: command)
-        print execute_com
+        def execute= sh(returnStdout: true, script: command)
+        print execute
 
     }
     else if(APP_TYPE == "java" ) {
         command =  "cp target/*.jar ${COMPONENT}.jar && zip -r ${FILENAME} ${COMPONENT}.jar"
-        def execute_com= sh(returnStdout: true, script: command)
-        print execute_com
+        def execute= sh(returnStdout: true, script: command)
+        print execute
     }
     else if (APP_TYPE == "GOLANG") {
         command = "zip -r ${FILENAME} login-ci main.go user.go tracing.go"
-        def execute_com= sh(returnStdout: true, script: command)
-        print execute_com
+        def execute= sh(returnStdout: true, script: command)
+        print execute
     }
 }
 
 def code(APP_TYPE,COMPONENT) {
     if(APP_TYPE == 'NODEJS') {
         command = "npm install"
-        def execute_com= sh(returnStdout: true, script: command)
-        print execute_com
+        def execute= sh(returnStdout: true, script: command)
+        print execute
     }
     else if(APP_TYPE == 'GOLANG') {
         command = "go get -d && go build"
-        def execute_com = sh(returnStdout: true, script: command)
-        print execute_com
+        def execute = sh(returnStdout: true, script: command)
+        print execute
     }
     else if(APP_TYPE == 'JAVA') {
         command = "mvn clean package"
-        def execute_com = sh(returnStdout: true, script: command)
-        print execute_com
+        def execute = sh(returnStdout: true, script: command)
+        print execute
     }
     else if(APP_TYPE == "NGINX") {
         command = "npm install && npm run build"
-        def execute_com = sh(returnStdout: true, script: command)
-        print execute_com
+        def execute = sh(returnStdout: true, script: command)
+        print execute
     }
 }
