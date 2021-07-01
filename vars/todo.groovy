5a90d6b65d5d5e6aeedeca8d90cd7647c8da77b8
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-07-01 17:44:36 +0530

    groovy

--- vars/todo.groovy
+++ vars/todo.groovy
@@ -1,7 +1,6 @@
-def call (Map params =  [:] )
-{
+def call (Map params =  [:] ) {
     def args = [
-            NEXUS_IP         : '172.31.9.137',
+            NEXUS_IP: '172.31.9.137',
     ]
     args << params
 
@@ -16,30 +15,32 @@ def call (Map params =  [:] )
             pollSCM('*/2 * * * 1-5')
         }
         environment {
-            COMPONENT    = "${args.COMPONENT}"
-            NEXUS_IP     = "${args.NEXUS_IP}"
+            COMPONENT = "${args.COMPONENT}"
+            NEXUS_IP = "${args.NEXUS_IP}"
             PROJECT_NAME = "${args.PROJECT_NAME}"
-            SLAVE_LABEL  = "${args.SLAVE_LABEL}"
-            APP_TYPE     =  "${args.APP_TYPE}"
+            SLAVE_LABEL = "${args.SLAVE_LABEL}"
+            APP_TYPE = "${args.APP_TYPE}"
         }
 
         stages {
-            stage ('Build code & install dependices') {
-                script {
-                    build = new nexus()
-                    build.code_build("${APP_TYPE}", "${COMPONENT}")
+            stage('Build code & install dependices') {
+                step {
+                    script {
+                        build = new nexus()
+                        build.code_build("${APP_TYPE}", "${COMPONENT}")
+                    }
                 }
             }
-        }
+
             stage('prepare artifacts') {
                 steps {
                     script {
                         prepare = new nexus()
-                        prepare.make_artifacts ("${APP_TYPE}","${COMPONENT}")
+                        prepare.make_artifacts("${APP_TYPE}", "${COMPONENT}")
                     }
                 }
             }
-            stage ('upload artifact') {
+            stage('upload artifact') {
                 steps {
                     script {
                         prepare = new nexus()
@@ -48,5 +49,7 @@ def call (Map params =  [:] )
 
                 }
             }
+        }
     }
 }
+
