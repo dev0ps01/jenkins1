414aded6f1740572996e4628b915aab3a0a5a977
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-07-08 11:29:44 +0530

    new

--- seed.groovy
+++ seed.groovy
@@ -42,3 +42,24 @@ for (i in 0..count) {
     }
 }
 
+pipelineJob("Deployment Pipeline") {
+    configure { flowdefinition ->
+        flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+            'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+                'userRemoteConfigs' {
+                    'hudson.plugins.git.UserRemoteConfig' {
+                        'url'('https://github.com/dev0ps01/jenkins1.git')
+                    }
+                }
+                'branches' {
+                    'hudson.plugins.git.BranchSpec' {
+                        'name'('main')
+                    }
+                }
+            }
+            'scriptPath'('Jenkinsfile-Deployment')
+            'lightweight'(true)
+        }
+    }
+}
+
