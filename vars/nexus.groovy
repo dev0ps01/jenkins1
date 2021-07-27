6475d503f1ab1f084cd5a2e0f7d6241feea98fd3
Vamsi Akkala <vamsi.a@zelarsoft.com>
2021-07-27 19:51:19 +0530

    new

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -51,8 +51,8 @@ def code_build(APP_TYPE,COMPONENT) {
         print execute_com
     }
     else if(APP_TYPE == "NGINX") {
-        command = " npm install && npm run build"
-        def execute_com = sh(returnStdout: true, script: command)
+        command = "npm install && npm run build"
+        def execute_com=sh(returnStdout: true, script: command)
         print execute_com
     }
 }
